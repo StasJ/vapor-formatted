77625f6548b10df7dcfd7fe12e863497e1cc8415
Scott Pearse <pearse@ucar.edu>
2017-06-15 10:14:56 -0600

    Fix for update-histo crash

--- apps/vaporgui/GeometryWidget.h
+++ apps/vaporgui/GeometryWidget.h
@@ -38,7 +38,8 @@ class GeometryWidget : public QWidget, public Ui_GeometryWidgetGUI {
     bool isContainer() const { return true; }
     void Update(VAPoR::ParamsMgr *paramsMgr, VAPoR::DataMgr *dataMgr, VAPoR::RenderParams *rParams);
 
-  public slots:
+    // public slots:
+  private slots:
     void setRange(double min, double max);
     void copyRegion();
     void updateRenTypeCombo();
