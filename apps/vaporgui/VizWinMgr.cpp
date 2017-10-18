d52bd16233746fde4c9d39ae4bf8e02598a6a88a
John Clyne <clyne@ucar.edu>
2017-10-17 20:46:45 -0600

    Incremental commit

--- apps/vaporgui/VizWinMgr.cpp
+++ apps/vaporgui/VizWinMgr.cpp
@@ -562,11 +562,8 @@ void VizWinMgr::UpdateRouters() {
         assert(w);
         w->setEnabled(true);
 
-        cout << "UPdating " << tab << endl;
-
         eRouter->updateTab();
     }
-    cout << "Done UPdating " << endl;
 
     // Now handle the active render event router.
     //
