8ac6275be4d03af63d775482f4ce37b476b6dce4
John Clyne <clyne@ucar.edu>
2017-10-27 12:15:37 -0600

    Added support to ParamsMgr for applications to register additional RenderParams classes associated with a data set

--- lib/render/ControlExecutive.cpp
+++ lib/render/ControlExecutive.cpp
@@ -16,10 +16,11 @@
 using namespace VAPoR;
 using namespace std;
 
-ControlExec::ControlExec(vector<string> appParamsNames, size_t cacheSizeMB, int nThreads)
+ControlExec::ControlExec(vector<string> appParamsNames, vector<string> appRenderParamNames,
+                         size_t cacheSizeMB, int nThreads)
     : MyBase() {
 
-    _paramsMgr = new ParamsMgr(appParamsNames);
+    _paramsMgr = new ParamsMgr(appParamsNames, appRenderParamNames);
     _dataStatus = new DataStatus(cacheSizeMB, nThreads);
     _shaderMgrs.clear();
     _visualizers.clear();
