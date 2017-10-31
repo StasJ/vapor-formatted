92bd436bd37bc178c1d218f919070e5cb5341a23
Scott Pearse <pearse@ucar.edu>
2017-10-31 12:10:56 -0600

    Refactored contour params and gui

--- apps/vaporgui/ContourSubtabs.h
+++ apps/vaporgui/ContourSubtabs.h
@@ -46,8 +46,9 @@ class ContourAppearanceSubtab : public QWidget, public Ui_ContourAppearanceGUI {
     void Initialize(VAPoR::ContourParams *cParams);
 
   private:
-    void SetIsovalues();
     double GetContourMinOrMax(string minOrMax);
+    void enableSliders();
+    void disableSliders();
 
     VAPoR::ContourParams *_cParams;
     VAPoR::DataMgr *_dataMgr;
@@ -58,6 +59,7 @@ class ContourAppearanceSubtab : public QWidget, public Ui_ContourAppearanceGUI {
     Combo *_spacingCombo;
 
   private slots:
+    void SetContourValues();
     void MappingChanged() { cout << "mapping changed!" << endl; }
 
     void EndTFChange();
