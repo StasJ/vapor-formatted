3fa50fc38b34398b9e6118628af835bc49e1e84c
Scott Pearse <pearse@ucar.edu>
2017-11-09 15:59:32 -0700

    Incremental

--- apps/vaporgui/TFWidget.h
+++ apps/vaporgui/TFWidget.h
@@ -60,7 +60,7 @@ class TFWidget : public QWidget, public Ui_TFWidgetGUI {
   public slots:
     // Made public, to be called from EventRouter::Initialize
     //
-    void setCMVar(const QString &);
+    void setColorMapping(const QString &);
 
   private slots:
     void fileSaveTF();
@@ -76,8 +76,10 @@ class TFWidget : public QWidget, public Ui_TFWidgetGUI {
   private:
     void collapseAutoUpdateHistoCheckbox();
     string getVariableName();
-    void setNativeTranferFunction(string var);
+    void setNativeTransferFunction(string var);
     void configureConstColorWidgets(string var);
+    void configureConstantColor(string var);
+    void configureColorMappingToVariable(string var);
     void connectWidgets();
     void updateSliders();
     void updateAutoUpdateHistoCheckbox();
