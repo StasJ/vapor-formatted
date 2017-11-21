69856a62333198accce46de3accc55289e08939e
John Clyne <clyne@ucar.edu>
2017-11-21 16:56:10 -0700

    Fixed issue_441

--- lib/params/ParamsMgr.cpp
+++ lib/params/ParamsMgr.cpp
@@ -1117,6 +1117,10 @@ void ParamsMgr::PMgrStateSave::Save(const XmlNode *node, string description) {
          << description << endl;
 #endif
 
+    // Clear redo stack
+    //
+    cleanStack(0, _redoStack);
+
     // Set state change flags and CBs
     //
     for (int i = 0; i < _stateChangeFlags.size(); i++) {
@@ -1171,6 +1175,10 @@ void ParamsMgr::PMgrStateSave::EndGroup() {
     // Delete oldest elements if needed
     //
     cleanStack(_stackSize, _undoStack);
+
+    // Clear redo stack
+    //
+    cleanStack(0, _redoStack);
 
     _undoStack.push_back(make_pair(desc, new XmlNode(*_rootNode)));
 
