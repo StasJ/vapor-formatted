e607e03687c4fa61fd568f6ef595b9d62647ca8f
John Clyne <clyne@ucar.edu>
2017-11-21 09:44:17 -0700

    Fixed issue_395

--- apps/vaporgui/MappingFrame.h
+++ apps/vaporgui/MappingFrame.h
@@ -360,6 +360,7 @@ class MappingFrame : public QGLWidget {
     const int _bottomGap;
     VAPoR::DataMgr *_dataMgr;
     VAPoR::RenderParams *_rParams;
+    bool _mousePressFlag;
     VAPoR::ParamsMgr *_paramsMgr;
 
     QStringList _axisTexts;
