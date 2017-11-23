9e7226b96188d4c564c9314437941709b59367ed
Stanislaw Jaroszynski <stasj@ucar.edu>
2017-11-22 19:44:32 -0500

    If TF is loaded without saved bounds (non existent or set to 0,0), it will be set to the bounds of the current variable

--- include/vapor/MapperFunction.h
+++ include/vapor/MapperFunction.h
@@ -60,8 +60,9 @@ class PARAMS_API MapperFunction : public ParamsBase {
 
     //! Load a transfer function from a file,
     //! \param[in] path Path of input file
+    //! \param[in] defaultDataBounds if data bounds not provided in TF file this will be used
     //
-    int LoadFromFile(string path);
+    int LoadFromFile(string path, vector<double> defaultDataBounds);
 
     // Get static string identifier for this params class
     //
