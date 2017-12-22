1e8da6a3a187eb8bfffff6ded2d118c78b29eb88
John Clyne <clyne@ucar.edu>
2017-12-22 10:59:07 -0700

    Incremental commit: 	Added GetTopologyDim() and GetGeometryDim() methods to DC and Grid 	classes, which return the topological and geometric dimensionality, 	respectively, of a variable mesh (DC case) or grid (Grid case)

--- lib/vdc/UnstructuredGrid.cpp
+++ lib/vdc/UnstructuredGrid.cpp
@@ -59,19 +59,14 @@ bool UnstructuredGrid::GetCellNodes(const std::vector<size_t> &cindices,
                                     std::vector<vector<size_t>> &nodes) const {
     nodes.clear();
 
-    vector<size_t> cdims = GetCellDimensions();
-    assert(cindices.size() == cdims.size());
+    vector<size_t> cCindices = cindices;
+    ClampCellIndex(cCindices);
 
-    // Check if invalid indices
-    //
-    for (int i = 0; i < cindices.size(); i++) {
-        if (cindices[i] > (cdims[i] - 1))
-            return (false);
-    }
+    vector<size_t> cdims = GetCellDimensions();
 
     // _vertexOnFace is dimensioned cdims[0] x _maxVertexPerFace
     //
-    const int *ptr = _vertexOnFace + (_maxVertexPerFace * cindices[0]);
+    const int *ptr = _vertexOnFace + (_maxVertexPerFace * cCindices[0]);
     long offset = GetNodeOffset();
 
     if (cdims.size() == 1) {
@@ -95,11 +90,11 @@ bool UnstructuredGrid::GetCellNodes(const std::vector<size_t> &cindices,
                 continue;
 
             indices.push_back(*ptr + offset);
-            indices.push_back(cindices[1]);
+            indices.push_back(cCindices[1]);
             nodes.push_back(indices);
         }
 
-        ptr = _vertexOnFace + (_maxVertexPerFace * cindices[0]);
+        ptr = _vertexOnFace + (_maxVertexPerFace * cCindices[0]);
         for (int i = 0; i < _maxVertexPerFace; i++) {
             vector<size_t> indices;
             if (*ptr == GetMissingID() || *ptr + offset < 0)
@@ -108,7 +103,7 @@ bool UnstructuredGrid::GetCellNodes(const std::vector<size_t> &cindices,
                 continue;
 
             indices.push_back(*ptr + offset);
-            indices.push_back(cindices[1] + 1);
+            indices.push_back(cCindices[1] + 1);
             nodes.push_back(indices);
         }
     }
@@ -119,19 +114,14 @@ bool UnstructuredGrid::GetCellNeighbors(const std::vector<size_t> &cindices,
                                         std::vector<vector<size_t>> &cells) const {
     cells.clear();
 
-    vector<size_t> cdims = GetCellDimensions();
-    assert(cindices.size() == cdims.size());
+    vector<size_t> cCindices = cindices;
+    ClampCellIndex(cCindices);
 
-    // Check if invalid indices
-    //
-    for (int i = 0; i < cindices.size(); i++) {
-        if (cindices[i] > (cdims[i] - 1))
-            return (false);
-    }
+    vector<size_t> cdims = GetCellDimensions();
 
     // _faceOnFace is dimensioned cdims[0] x _maxVertexPerFace
     //
-    const int *ptr = _faceOnFace + (_maxVertexPerFace * cindices[0]);
+    const int *ptr = _faceOnFace + (_maxVertexPerFace * cCindices[0]);
     long offset = GetCellOffset();
 
     if (cdims.size() == 1) {
@@ -142,7 +132,7 @@ bool UnstructuredGrid::GetCellNeighbors(const std::vector<size_t> &cindices,
 
             if (*ptr != GetBoundaryID()) {
                 indices.push_back(*ptr + offset);
-                indices.push_back(cindices[1]);
+                indices.push_back(cCindices[1]);
             }
             cells.push_back(indices);
         }
@@ -155,7 +145,7 @@ bool UnstructuredGrid::GetCellNeighbors(const std::vector<size_t> &cindices,
 
             if (*ptr != GetBoundaryID()) {
                 indices.push_back(*ptr + offset);
-                indices.push_back(cindices[1]);
+                indices.push_back(cCindices[1]);
             }
 
             cells.push_back(indices);
@@ -163,9 +153,9 @@ bool UnstructuredGrid::GetCellNeighbors(const std::vector<size_t> &cindices,
 
         // layer below
         //
-        if (cindices[1] != 0) {
-            vector<size_t> indices = cindices;
-            indices[1] = cindices[1] - 1;
+        if (cCindices[1] != 0) {
+            vector<size_t> indices = cCindices;
+            indices[1] = cCindices[1] - 1;
             cells.push_back(indices);
         } else {
             cells.push_back(vector<size_t>());
@@ -173,8 +163,8 @@ bool UnstructuredGrid::GetCellNeighbors(const std::vector<size_t> &cindices,
 
         // layer above
         //
-        if (cindices[1] != cdims[1] - 1) {
-            vector<size_t> indices = cindices;
+        if (cCindices[1] != cdims[1] - 1) {
+            vector<size_t> indices = cCindices;
             indices[1] = indices[1] + 1;
             cells.push_back(indices);
         } else {
