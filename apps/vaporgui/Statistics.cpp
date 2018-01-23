f1761508a7430d4b4d148c825c04cee6f9d1a699
Scott Pearse <pearse@ucar.edu>
2018-01-23 09:28:38 -0700

    Incremental commit.  Axes now drawing.  Up next: Annotations, initialization sequence, and swapping between lat/lon and native units (meters, etc)

--- apps/vaporgui/Statistics.cpp
+++ apps/vaporgui/Statistics.cpp
@@ -60,7 +60,7 @@ Statistics::~Statistics() {
 
 bool Statistics::Update() {
     // Initialize pointers
-    VAPoR::DataStatus *dataStatus = _controlExec->getDataStatus();
+    VAPoR::DataStatus *dataStatus = _controlExec->GetDataStatus();
     std::vector<std::string> dmNames = dataStatus->GetDataMgrNames();
     if (dmNames.empty()) {
         this->close();
@@ -255,7 +255,7 @@ bool Statistics::Update() {
 
 void Statistics::_updateStatsTable() {
     // Initialize pointers
-    VAPoR::DataStatus *dataStatus = _controlExec->getDataStatus();
+    VAPoR::DataStatus *dataStatus = _controlExec->GetDataStatus();
     GUIStateParams *guiParams = dynamic_cast<GUIStateParams *>(
         _controlExec->GetParamsMgr()->GetParams(GUIStateParams::GetClassType()));
     std::string currentDatasetName = guiParams->GetStatsDatasetName();
@@ -389,7 +389,7 @@ int Statistics::initControlExec(ControlExec *ce) {
         return -1;
 
     // Store the active dataset name
-    std::vector<std::string> dmNames = _controlExec->getDataStatus()->GetDataMgrNames();
+    std::vector<std::string> dmNames = _controlExec->GetDataStatus()->GetDataMgrNames();
     if (dmNames.empty())
         return -1;
     else {
@@ -739,7 +739,7 @@ bool Statistics::_calc3M(std::string varname) {
     std::string dsName = guiParams->GetStatsDatasetName();
     StatisticsParams *statsParams = dynamic_cast<StatisticsParams *>(
         _controlExec->GetParamsMgr()->GetAppRenderParams(dsName, StatisticsParams::GetClassType()));
-    VAPoR::DataMgr *currentDmgr = _controlExec->getDataStatus()->GetDataMgr(dsName);
+    VAPoR::DataMgr *currentDmgr = _controlExec->GetDataStatus()->GetDataMgr(dsName);
 
     int minTS = statsParams->GetCurrentMinTS();
     int maxTS = statsParams->GetCurrentMaxTS();
@@ -795,7 +795,7 @@ bool Statistics::_calcMedian(std::string varname) {
     std::string dsName = guiParams->GetStatsDatasetName();
     StatisticsParams *statsParams = dynamic_cast<StatisticsParams *>(
         _controlExec->GetParamsMgr()->GetAppRenderParams(dsName, StatisticsParams::GetClassType()));
-    VAPoR::DataMgr *currentDmgr = _controlExec->getDataStatus()->GetDataMgr(dsName);
+    VAPoR::DataMgr *currentDmgr = _controlExec->GetDataStatus()->GetDataMgr(dsName);
 
     int minTS = statsParams->GetCurrentMinTS();
     int maxTS = statsParams->GetCurrentMaxTS();
@@ -838,7 +838,7 @@ bool Statistics::_calcStddev(std::string varname) {
     std::string dsName = guiParams->GetStatsDatasetName();
     StatisticsParams *statsParams = dynamic_cast<StatisticsParams *>(
         _controlExec->GetParamsMgr()->GetAppRenderParams(dsName, StatisticsParams::GetClassType()));
-    VAPoR::DataMgr *currentDmgr = _controlExec->getDataStatus()->GetDataMgr(dsName);
+    VAPoR::DataMgr *currentDmgr = _controlExec->GetDataStatus()->GetDataMgr(dsName);
 
     int minTS = statsParams->GetCurrentMinTS();
     int maxTS = statsParams->GetCurrentMaxTS();
@@ -1104,7 +1104,7 @@ void Statistics::_exportTextClicked() {
         StatisticsParams *statsParams =
             dynamic_cast<StatisticsParams *>(_controlExec->GetParamsMgr()->GetAppRenderParams(
                 dsName, StatisticsParams::GetClassType()));
-        VAPoR::DataMgr *currentDmgr = _controlExec->getDataStatus()->GetDataMgr(dsName);
+        VAPoR::DataMgr *currentDmgr = _controlExec->GetDataStatus()->GetDataMgr(dsName);
         std::vector<std::string> availVars3D = currentDmgr->GetDataVarNames(3, true);
 
         file << "#Variable Statistics" << endl;
