711412b62528246566d634c1c0c87c8da8bf6d2f
Scott Pearse <pearse@ucar.edu>
2018-01-24 15:51:26 -0700

    Incremental commit.  ToDo: Label orientatios in TextObject, and swapping between native and lat/lon coordinates

--- apps/vaporgui/VizFeatureEventRouter.cpp
+++ apps/vaporgui/VizFeatureEventRouter.cpp
@@ -752,6 +752,7 @@ void VizFeatureEventRouter::drawTimeStep(string myString) {
     float color[3];
     mp->GetTimeAnnotColor(color);
 
+    cout << "Drawing timestep" << endl;
     _controlExec->DrawText(myString, x, y, size, color, 1);
 }
 
