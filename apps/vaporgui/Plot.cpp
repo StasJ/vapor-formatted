d9a42e2b5342d43d074d437a8b4f9eb610ece22d
Samuel Li <shaomeng@ucar.edu>
2018-01-26 17:37:43 -0700

    WIP

--- apps/vaporgui/Plot.cpp
+++ apps/vaporgui/Plot.cpp
@@ -33,14 +33,16 @@ Plot::Plot(QWidget *parent) {
 
     setupUi(this);
     setWindowTitle("Plot Utility");
+    P1P2Widget->setTabText(0, QString::fromAscii("Point 1 Position"));
+    P1P2Widget->setTabText(1, QString::fromAscii("Point 2 Position"));
     P1Widget->Reinit(GeometryWidget::THREED, GeometryWidget::SINGLEPOINT,
-                     // GeometryWidget::MINMAX,
                      GeometryWidget::AUXILIARY);
     P2Widget->Reinit(GeometryWidget::THREED, GeometryWidget::SINGLEPOINT,
-                     // GeometryWidget::MINMAX,
                      GeometryWidget::AUXILIARY);
+    P1Widget->hideSinglePointTabHeader();
+    P2Widget->hideSinglePointTabHeader();
 
-    // MyFidelityWidget->Reinit(FidelityWidget::AUXILIARY);
+    MyFidelityWidget->Reinit(FidelityWidget::AUXILIARY);
 
     Connect();
 }
@@ -162,6 +164,9 @@ void Plot::Update() {
     VariablesTable->update();
     VariablesTable->repaint();
     VariablesTable->viewport()->update();
+
+    // Update LOD, Refinement
+    MyFidelityWidget->Update(currentDmgr, _controlExec->GetParamsMgr(), statsParams);
 
     // Update geometry extents
     P1Widget->Update(_controlExec->GetParamsMgr(), currentDmgr, plotParams);
