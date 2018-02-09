5b8e264854a858783ab7836df8e847420e914c8d
Scott Pearse <pearse@ucar.edu>
2018-02-09 09:38:24 -0700

    Commit before bug report #549

--- apps/vaporgui/VizFeatureEventRouter.cpp
+++ apps/vaporgui/VizFeatureEventRouter.cpp
@@ -77,7 +77,7 @@ void VizFeatureEventRouter::connectAnnotationWidgets() {
     connect(_ticWidthCombo, SIGNAL(valueChanged(double)), this, SLOT(setAxisTicWidth(double)));
     connect(axisColorButton, SIGNAL(pressed()), this, SLOT(setAxisColor()));
     connect(axisBackgroundColorButton, SIGNAL(pressed()), this, SLOT(setAxisBackgroundColor()));
-    connect(_annotationVaporTable, SIGNAL(valueChanged(int, int)), this,
+    connect(_annotationVaporTable, SIGNAL(returnPressed()), this,
             SLOT(axisAnnotationTableChanged()));
     connect(xTicOrientationCombo, SIGNAL(activated(int)), this, SLOT(setXTicOrientation(int)));
     connect(yTicOrientationCombo, SIGNAL(activated(int)), this, SLOT(setYTicOrientation(int)));
@@ -268,8 +268,10 @@ void VizFeatureEventRouter::updateDataMgrCombo() {
     int index = dataMgrSelectorCombo->findText(qCurrentSelection);
     if (index > 0)
         dataMgrSelectorCombo->setCurrentIndex(index);
-    else
+    else {
         dataMgrSelectorCombo->setCurrentIndex(0);
+        setCurrentAxisDataMgr(0);
+    }
 }
 
 void VizFeatureEventRouter::updateAnnotationCheckbox() {
@@ -487,7 +489,8 @@ void VizFeatureEventRouter::initializeAnnotationExtents(AxisAnnotation *aa) {
 
     VizFeatureParams *vfParams = (VizFeatureParams *)GetActiveParams();
     string dataMgr = vfParams->GetCurrentAxisDataMgrName();
-    aa->SetAxisDataMgr(dataMgr);
+    cout << "initializing axis for " << dataMgr << endl;
+    aa->SetDataMgrName(dataMgr);
 }
 
 void VizFeatureEventRouter::initializeAnnotation(AxisAnnotation *aa) {
@@ -566,7 +569,6 @@ void VizFeatureEventRouter::setCurrentAxisDataMgr(int index) {
 
     VizFeatureParams *vfParams = (VizFeatureParams *)GetActiveParams();
     vfParams->SetCurrentAxisDataMgrName(dataMgr);
-    cout << "Setting dm to " << dataMgr << endl;
 }
 
 vector<double> VizFeatureEventRouter::getTableRow(int row) {
@@ -785,7 +787,6 @@ void VizFeatureEventRouter::drawTimeStep(string myString) {
     float color[3];
     mp->GetTimeAnnotColor(color);
 
-    cout << "Drawing timestep" << endl;
     _controlExec->DrawText(myString, x, y, size, color, 1);
 }
 
