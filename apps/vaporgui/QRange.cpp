c2905b1e42c24faf709709bf56d8a15c14b7d5d8
Samuel Li <shaomenglee@gmail.com>
2018-02-22 17:54:36 -0700

    reuse combo

--- apps/vaporgui/QRange.cpp
+++ apps/vaporgui/QRange.cpp
@@ -9,6 +9,8 @@ QRange::QRange(QWidget *parent) : QWidget(parent), _ui(new Ui::QRange) {
 
     connect(_ui->minSliderEdit, SIGNAL(valueChanged(double)), this, SLOT(_minChanged(double)));
     connect(_ui->maxSliderEdit, SIGNAL(valueChanged(double)), this, SLOT(_maxChanged(double)));
+    connect(_ui->minSliderEdit, SIGNAL(valueChanged(int)), this, SLOT(_minChanged(int)));
+    connect(_ui->maxSliderEdit, SIGNAL(valueChanged(int)), this, SLOT(_maxChanged(int)));
 }
 
 QRange::~QRange() { delete _ui; }
@@ -28,14 +30,24 @@ void QRange::GetValue(double &min, double &max) {
 void QRange::_minChanged(double value) {
     if (value > _ui->maxSliderEdit->GetCurrentValue())
         _ui->maxSliderEdit->SetValue(value);
-
     emit rangeChanged();
 }
 
 void QRange::_maxChanged(double value) {
     if (value < _ui->minSliderEdit->GetCurrentValue())
         _ui->minSliderEdit->SetValue(value);
+    emit rangeChanged();
+}
 
+void QRange::_minChanged(int value) {
+    if ((double)value > _ui->maxSliderEdit->GetCurrentValue())
+        _ui->maxSliderEdit->SetValue((double)value);
+    emit rangeChanged();
+}
+
+void QRange::_maxChanged(int value) {
+    if ((double)value < _ui->minSliderEdit->GetCurrentValue())
+        _ui->minSliderEdit->SetValue((double)value);
     emit rangeChanged();
 }
 
@@ -44,6 +56,11 @@ void QRange::SetMainLabel(const QString &label) { _ui->mainLabel->setText(label)
 void QRange::SetDecimals(int dec) {
     _ui->minSliderEdit->SetDecimals(dec);
     _ui->maxSliderEdit->SetDecimals(dec);
+}
+
+void QRange::SetIntType(bool val) {
+    _ui->minSliderEdit->SetIntType(val);
+    _ui->maxSliderEdit->SetIntType(val);
 }
 
 void QRange::SetValue(double smallVal, double bigVal) {
