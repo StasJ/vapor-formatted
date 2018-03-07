c0355176fc5ebef15f88001a21f8f3e2d481c02e
Samuel Li <shaomenglee@gmail.com>
2018-03-07 15:57:15 -0700

    writing single images now support PNG

--- apps/vaporgui/MainForm.cpp
+++ apps/vaporgui/MainForm.cpp
@@ -1965,7 +1965,7 @@ void MainForm::captureSingleJpeg() {
     string imageDir = p->GetCurrentImageSavePath();
 
     QFileDialog fileDialog(this, "Specify single image capture file name", imageDir.c_str(),
-                           "Jpeg images (*.jpg *.jpeg)");
+                           "PNG or JPEG images (*.png *.jpg *.jpeg)");
     fileDialog.setAcceptMode(QFileDialog::AcceptSave);
     fileDialog.move(pos());
     fileDialog.resize(450, 450);
@@ -1980,8 +1980,8 @@ void MainForm::captureSingleJpeg() {
     // Extract the path, and the root name, from the returned string.
     QFileInfo *fileInfo = new QFileInfo(fn);
     QString suffix = fileInfo->suffix();
-    if (suffix != "jpg" && suffix != "jpeg") {
-        MSG_ERR("Image capture file name must end with .jpg or .jpeg");
+    if (suffix != "jpg" && suffix != "jpeg" && suffix != "png") {
+        MSG_ERR("Image capture file name must end with .jpg or .jpeg or .png");
         return;
     }
 
