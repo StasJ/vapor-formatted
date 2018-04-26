a1d7d5640c01383f810e212cc8e04b49d9fffc28
Samuel Li <shaomeng@ucar.edu>
2018-04-26 16:55:45 -0600

    there are still some unused-variable warning

--- apps/vaporgui/ErrorReporter.cpp
+++ apps/vaporgui/ErrorReporter.cpp
@@ -136,7 +136,7 @@ void ErrorReporter::Report(string msg, Type severity, string details) {
         break;
     }
 
-    int ret = box.exec();
+    box.exec();
     QAbstractButton *clicked = box.clickedButton();
     QMessageBox::ButtonRole role = box.buttonRole(clicked);
 
@@ -186,7 +186,7 @@ string ErrorReporter::GetSystemInformation() {
            "\n";
     ret += "Distro:\n";
     char buffer[128];
-    FILE *pipe = popen("lsb_release", "r"), pclose;
+    FILE *pipe = popen("lsb_release", "r");
     if (pipe) {
         while (!feof(pipe)) {
             if (fgets(buffer, 128, pipe) != 0)
