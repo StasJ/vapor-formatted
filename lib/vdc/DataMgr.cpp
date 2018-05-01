eb2b46bb9e2044e40f9cd78edec0404813528b7a
John Clyne <clyne@ucar.edu>
2018-05-01 16:22:11 -0600

    Fixed issue_743

--- lib/vdc/DataMgr.cpp
+++ lib/vdc/DataMgr.cpp
@@ -2673,7 +2673,7 @@ DataMgr::_make_grid_curvilinear(size_t ts, int level, int lod,
     RegularGrid xrg(dims2d, bs2d, xcblkptrs, minu2d, maxu2d);
     RegularGrid yrg(dims2d, bs2d, ycblkptrs, minu2d, maxu2d);
 
-    const KDTreeRG *kdtree = _getKDTree2D(ts, level, lod, cvarsinfo, xrg, yrg);
+    const KDTreeRG *kdtree = _getKDTree2D(ts, level, lod, cvarsinfo, xrg, yrg, bmin, bmax);
 
     CurvilinearGrid *g = new CurvilinearGrid(dims, bs, blkptrs, xrg, yrg, zcoords, kdtree);
 
@@ -2831,7 +2831,7 @@ UnstructuredGrid2D *DataMgr::_make_grid_unstructured2d(
 
     UnstructuredGridCoordless zug;
 
-    const KDTreeRG *kdtree = _getKDTree2D(ts, level, lod, cvarsinfo, xug, yug);
+    const KDTreeRG *kdtree = _getKDTree2D(ts, level, lod, cvarsinfo, xug, yug, bmin, bmax);
 
     UnstructuredGrid2D *g = new UnstructuredGrid2D(
         vertexDims, faceDims, edgeDims, bs, blkptrs, vertexOnFace, faceOnVertex, faceOnFace,
@@ -3144,7 +3144,8 @@ void DataMgr::_unlock_blocks(const void *blks) {
 
 const KDTreeRG *DataMgr::_getKDTree2D(size_t ts, int level, int lod,
                                       const vector<DC::CoordVar> &cvarsinfo, const Grid &xg,
-                                      const Grid &yg) {
+                                      const Grid &yg, const vector<size_t> &bmin,
+                                      const vector<size_t> &bmax) {
     assert(cvarsinfo.size() >= 2);
     assert(xg.GetDimensions() == yg.GetDimensions());
 
@@ -3153,7 +3154,11 @@ const KDTreeRG *DataMgr::_getKDTree2D(size_t ts, int level, int lod,
         varnames.push_back(cvarsinfo[i].GetName());
     }
 
-    const string key = "KDTree";
+    string key = "KDTree";
+    key += ":";
+    key += vector_to_string(bmin);
+    key += ":";
+    key += vector_to_string(bmax);
 
     KDTreeRG *kdtree = NULL;
 
