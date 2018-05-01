8e2be19496033afda6bf310b88866e5d14211390
Stas Jaroszynski <stasj@ucar.edu>
2018-04-30 19:00:42 -0600

    Working

--- apps/vaporgui/windowsUtils.h
+++ apps/vaporgui/windowsUtils.h
@@ -5,6 +5,7 @@
 #include <Windows.h>
 
 #define WINDOWS_SUCCESS ERROR_SUCCESS
+#define WINDOWS_ERROR_FILE_NOT_FOUND ERROR_FILE_NOT_FOUND
 #define WINDOWS_HKEY_CLASSES_ROOT HKEY_CLASSES_ROOT
 #define WINDOWS_HKEY_CURRENT_CONFIG HKEY_CURRENT_CONFIG
 #define WINDOWS_HKEY_CURRENT_USER HKEY_CURRENT_USER
@@ -18,8 +19,6 @@ LONG Windows_GetRegistryString(HKEY hKey, const std::string &strValueName, std::
 LONG Windows_SetRegistryString(HKEY hKey, const std::string &strValueName,
                                const std::string &strValue);
 std::string Windows_GetErrorString(LONG errorCode);
-
-void Windows_setLog(FILE *f);
 
 #endif
 #endif
