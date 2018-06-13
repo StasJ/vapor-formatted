0e2398ecfa91e7df20ba9119eccc122877d6dc34
Samuel Li <shaomeng@ucar.edu>
2018-06-13 10:58:55 -0600

    clean up comments

--- lib/render/ContourRenderer.cpp
+++ lib/render/ContourRenderer.cpp
@@ -24,17 +24,15 @@
 
 #include <vapor/glutil.h> // Must be included first!!!
 
-#include <vapor/ContourRenderer.h>
-#include <vapor/Renderer.h>
-#include <vapor/Visualizer.h>
-//#include <vapor/params.h>
-#include <vapor/ContourParams.h>
-//#include <vapor/AnimationParams.h>
 #include "vapor/debug.h"
+#include <vapor/ContourParams.h>
+#include <vapor/ContourRenderer.h>
 #include <vapor/ControlExecutive.h>
 #include <vapor/DataStatus.h>
 #include <vapor/GetAppPath.h>
+#include <vapor/Renderer.h>
 #include <vapor/ViewpointParams.h>
+#include <vapor/Visualizer.h>
 #include <vapor/errorcodes.h>
 #include <vapor/regionparams.h>
 
@@ -150,7 +148,6 @@ int ContourRenderer::_buildCache() {
             _dataMgr->GetVariable(_cacheParams.ts, _cacheParams.heightVarName, _cacheParams.level,
                                   _cacheParams.lod, _cacheParams.boxMin, _cacheParams.boxMax);
     }
-    // StructuredGrid *sGrid = dynamic_cast<StructuredGrid *>(grid);
 
     if (grid == NULL || (heightGrid == NULL && !_cacheParams.heightVarName.empty())) {
         glEndList();
