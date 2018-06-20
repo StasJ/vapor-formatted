5ac5cd2ff50a83847143f19c0164f4d1ee830f88
Samuel Li <shaomeng@ucar.edu>
2018-06-20 17:51:09 -0600

    sync

--- include/vapor/DirectVolumeRenderer.h
+++ include/vapor/DirectVolumeRenderer.h
@@ -70,6 +70,8 @@ class RENDER_API DirectVolumeRenderer : public Renderer {
     GLuint _colormapTextureUnit; // GL_TEXTURE1
     // GLuint              _coordmapTextureUnit;         // GL_TEXTURE2 ??
     GLuint _volumeCoordinateTextureUnit; // GL_TEXTURE3
+    GLuint _frameBufferId;
+    GLuint _baskFaceTextureId;
 
     //
     // Draw faces using triangle strips
@@ -82,6 +84,16 @@ class RENDER_API DirectVolumeRenderer : public Renderer {
                                   const double *volumeMax,
                                   const size_t *dims, // num. of grid points
                                   bool frontFacing);
+
+    //
+    // Implement two pass ray caster algorithm here.
+    //
+    void _twoPassDVR();
+
+    //
+    // Texture initializations
+    //
+    void _initializeTextures();
 
 }; // End of class DirectVolumeRenderer
 
