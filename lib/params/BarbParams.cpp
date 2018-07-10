ebf776efd58be2c75b260023c306383ec947cffd
Scott Pearse <pearse@ucar.edu>
2018-07-10 11:39:32 -0600

    Increased default rake grid to 10x10, and decreased the default barb length to .1

--- lib/params/BarbParams.cpp
+++ lib/params/BarbParams.cpp
@@ -79,9 +79,6 @@ void BarbParams::_init() {
     float rgb[] = {1.f, 1.f, 1.f};
     SetConstantColor(rgb);
 
-    int grid[] = {4, 4, 1};
-    SetGrid(grid);
-
     // Crap. No error handling from constructor. Need Initialization()
     // method.
     //
