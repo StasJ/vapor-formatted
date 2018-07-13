7411e70cfed2254b6311fb14dcc7e404f90385f6
John Clyne <clyne@ucar.edu>
2018-07-13 16:14:25 -0600

    Increment

--- lib/vdc/DataMgr.cpp
+++ lib/vdc/DataMgr.cpp
@@ -387,6 +387,9 @@ int DataMgr::_parseOptions(vector<string> &options) {
         }
         i++;
     }
+
+    _doTransformVertical = false;
+    cout << "DO VERTICAL HARDODED TO " << _doTransformVertical << endl;
     options = newOptions;
 
     if (!ok) {
@@ -463,7 +466,11 @@ int DataMgr::Initialize(const vector<string> &files, const std::vector<string> &
 bool DataMgr::GetMesh(string meshname, DC::Mesh &m) const {
     assert(_dc);
 
-    bool ok = _dc->GetMesh(meshname, m);
+    bool ok = _dvm.GetMesh(meshname, m);
+    if (!ok) {
+        ok = _dc->GetMesh(meshname, m);
+    }
+
     if (!ok)
         return (ok);
 
@@ -492,12 +499,14 @@ bool DataMgr::GetMesh(string meshname, DC::Mesh &m) const {
         _assignHorizontalCoords(coord_vars);
     }
 
+#ifdef DEAD
     // if requested, replace native vertical coordiate variables
     // with derived coordinate variables
     //
     if (_doTransformVertical) {
         _assignVerticalCoords(coord_vars);
     }
+#endif
 
     m.SetCoordVars(coord_vars);
 
@@ -2880,6 +2889,7 @@ void DataMgr::_assignHorizontalCoords(vector<string> &coord_vars) const {
     }
 }
 
+#ifdef DEAD
 void DataMgr::_assignVerticalCoords(vector<string> &coord_vars) const {
 
     for (int i = 0; i < coord_vars.size(); i++) {
@@ -2892,6 +2902,7 @@ void DataMgr::_assignVerticalCoords(vector<string> &coord_vars) const {
         }
     }
 }
+#endif
 
 void DataMgr::_assignTimeCoord(string &coord_var) const {
 
@@ -3375,9 +3386,6 @@ int DataMgr::_initVerticalCoordVars() {
     vector<string> coordvars;
     for (int i = 0; i < meshnames.size(); i++) {
 
-        if (!_hasHorizontalXForm(meshnames[i]))
-            continue;
-
         string standard_name, formula_terms;
         if (!_hasVerticalXForm(meshnames[i], standard_name, formula_terms)) {
             continue;
@@ -3392,6 +3400,7 @@ int DataMgr::_initVerticalCoordVars() {
 
         cout << "meshname, coord var name " << meshnames[i] << " " << m.GetCoordVars()[2] << endl;
 
+#ifdef DEAD
         coordvars.push_back(m.GetCoordVars()[2]);
 
         vector<string> derivedCoordvars = coordvars;
@@ -3402,6 +3411,7 @@ int DataMgr::_initVerticalCoordVars() {
         if (_getDerivedCoordVar(derivedCoordvars[0]))
             continue;
 
+#endif
         DerivedCoordVarStandardWRF_Terrain *derivedVar =
             new DerivedCoordVarStandardWRF_Terrain(_dc, meshnames[i], formula_terms);
 
@@ -3412,6 +3422,12 @@ int DataMgr::_initVerticalCoordVars() {
         }
 
         _dvm.AddCoordVar(derivedVar);
+
+        vector<string> coord_vars = m.GetCoordVars();
+        coord_vars[2] = derivedVar->GetName();
+        m.SetCoordVars(coord_vars);
+
+        _dvm.AddMesh(m);
     }
 
     return (0);
