fb3e1c49918e9410a5a7d44d4368d923b54e386a
Scott Pearse <pearse@ucar.edu>
2018-08-07 14:18:31 -0600

    fixed typo

--- apps/vaporgui/TFWidget.cpp
+++ apps/vaporgui/TFWidget.cpp
@@ -70,17 +70,17 @@ void TFWidget::collapseConstColorWidgets() {
 void TFWidget::showConstColorWidgets() {
     useConstColorFrame->show();
     constColorFrame->show();
-    adjsutSize();
+    adjustSize();
 }
 
 void TFWidget::hideWhitespaceFrame() {
     whitespaceFrame->hide();
-    adjsutSize();
+    adjustSize();
 }
 
 void TFWidget::showWhitespaceFrame() {
     whitespaceFrame->show();
-    adjsutSize();
+    adjustSize();
 }
 
 void TFWidget::Reinit(Flags flags) {
@@ -89,7 +89,7 @@ void TFWidget::Reinit(Flags flags) {
         showConstColorWidgets();
     else
         collapseConstColorWidgets();
-    adjsutSize();
+    adjustSize();
 }
 
 TFWidget::~TFWidget() {
