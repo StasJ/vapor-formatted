d3645f14bd76c5a35a1ae5507ac4eff6888a995b
John Clyne <clyne@ucar.edu>
2018-08-07 21:35:00 -0600

    Increment

--- lib/vdc/DerivedVar.cpp
+++ lib/vdc/DerivedVar.cpp
@@ -1521,6 +1521,7 @@ int DerivedCoordVar_UnStaggered::GetDimLensAtLevel(int level, std::vector<size_t
         return (-1);
 
     dims_at_level[_stagDim] -= 1;
+    bs_at_level[_stagDim] -= 1;
 
     return (0);
 }
