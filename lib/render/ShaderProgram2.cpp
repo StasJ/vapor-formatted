b4b949e6a8e116b909d0878b2fbb084b2bd8ac82
Stanislaw Jaroszynski <stasj@ucar.edu>
2018-08-28 19:27:08 -0600

    Added utility function

--- lib/render/ShaderProgram2.cpp
+++ lib/render/ShaderProgram2.cpp
@@ -18,6 +18,7 @@ ShaderProgram2::~ShaderProgram2() {
 bool ShaderProgram2::Link() {
     assert(!_linked);
     _id = glCreateProgram();
+    assert(_id);
     for (auto it = _shaders.begin(); it != _shaders.end(); it++) {
         assert((*it)->WasCompilationSuccessful());
         glAttachShader(_id, (*it)->GetID());
@@ -38,13 +39,15 @@ void ShaderProgram2::Bind() {
     glUseProgram(_id);
 }
 
-bool ShaderProgram2::IsBound() const {
-    int currentlyBoundProgramId;
-    glGetIntegerv(GL_CURRENT_PROGRAM, &currentlyBoundProgramId);
-    return _id == currentlyBoundProgramId;
-}
+bool ShaderProgram2::IsBound() const { return _id == GetBoundProgramID(); }
 
 void ShaderProgram2::UnBind() { glUseProgram(0); }
+
+int ShaderProgram2::GetBoundProgramID() {
+    int currentlyBoundProgramId;
+    glGetIntegerv(GL_CURRENT_PROGRAM, &currentlyBoundProgramId);
+    return currentlyBoundProgramId;
+}
 
 void ShaderProgram2::AddShader(Shader *s) { _shaders.push_back(s); }
 
