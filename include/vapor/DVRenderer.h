5adb2997717c1da44e12c6c8459a15881572868e
Samuel Li <shaomeng@ucar.edu>
2018-08-30 15:52:11 -0600

    create derived DVR classes

---
+++ include/vapor/DVRenderer.h
@@ -0,0 +1,17 @@
+#ifndef DVRENDERER_H
+#define DVRENDERER_H
+
+#include "vapor/DVRParams.h"
+#include "vapor/RayCaster.h"
+
+using namespace VAPoR;
+
+class RENDER_API DVRenderer : public RayCaster {
+  public:
+    DVRenderer(const ParamsMgr *pm, std::string &winName, std::string &dataSetName,
+               std::string &instName, DataMgr *dataMgr);
+
+    static std::string GetClassType() { return ("DVR"); }
+};
+
+#endif
