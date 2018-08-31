00e1173b26fa2b935cf0dcbafa538a0552df9478
Samuel Li <shaomeng@ucar.edu>
2018-08-31 14:39:25 -0600

    Create IsoSurfaceParams and IsoSurfaceRenderer class

---
+++ include/vapor/IsoSurfaceRenderer.h
@@ -0,0 +1,22 @@
+#ifndef ISOSURFACERENDERER_H
+#define ISOSURFACERENDERER_H
+
+#include "vapor/IsoSurfaceParams.h"
+#include "vapor/RayCaster.h"
+
+namespace VAPoR {
+
+class RENDER_API IsoSurfaceRenderer : public RayCaster {
+  public:
+    IsoSurfaceRnderer(const ParamsMgr *pm, std::string &winName, std::string &dataSetName,
+                      std::string &instName, DataMgr *dataMgr);
+
+    static std::string GetClassType() { return ("IsoSurface"); }
+
+  protected:
+    void _loadShaders();
+};
+
+}; // namespace VAPoR
+
+#endif
