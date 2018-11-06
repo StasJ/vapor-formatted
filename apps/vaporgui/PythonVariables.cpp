bdb455bfd17d20dd41e85403169fdc2ab810ddb5
Scott Pearse <pearse@ucar.edu>
2018-11-06 09:39:25 -0700

    Bug fixes and cleanup for coordinate variable support

--- apps/vaporgui/PythonVariables.cpp
+++ apps/vaporgui/PythonVariables.cpp
@@ -62,6 +62,9 @@ PythonVariables::PythonVariables(QWidget *parent) : QDialog(parent), Ui_PythonVa
     QPixmap thumbnail(pythonImagePath.c_str());
     _pythonLabel->setPixmap(thumbnail);
 
+    _variableTabs->removeTab(0);
+    ;
+
     _coordInputVarTable = new VaporTable(_coordVarTable, false, true);
     _coordInputVarTable->Reinit((VaporTable::ValidatorFlags)(0),
                                 (VaporTable::MutabilityFlags)(VaporTable::IMMUTABLE),
@@ -95,7 +98,7 @@ PythonVariables::PythonVariables(QWidget *parent) : QDialog(parent), Ui_PythonVa
 PythonVariables::~PythonVariables() {
     if (_coordInputVarTable) {
         delete _coordInputVarTable;
-        _coordVarInputTable = nullptr;
+        _coordInputVarTable = nullptr;
     }
     if (_2DInputVarTable) {
         delete _2DInputVarTable;
@@ -139,7 +142,7 @@ void PythonVariables::Update(bool internalUpdate) {
     _outputVarTable->blockSignals(true);
 
     numRows = _coordVars.size();
-    _coordVarTable->Update(numRows, numCols, tableValuesCoords);
+    _coordInputVarTable->Update(numRows, numCols, tableValuesCoords);
 
     numRows = _2DVars.size();
     _2DInputVarTable->Update(numRows, numCols, tableValues2D);
@@ -226,6 +229,9 @@ void PythonVariables::_connectWidgets() {
     connect(_3DInputVarTable, SIGNAL(valueChanged(int, int)), this,
             SLOT(_3DInputVarChanged(int, int)));
 
+    connect(_coordinatesCheckbox, SIGNAL(stateChanged(int)), this,
+            SLOT(_coordinatesCheckboxClicked(int)));
+
     connect(_scriptEdit, SIGNAL(textChanged()), this, SLOT(_scriptChanged()));
 
     connect(_newOutVarButton, SIGNAL(clicked()), this, SLOT(_createNewVariable()));
@@ -272,8 +278,8 @@ void PythonVariables::_newScript() {
 
         VAPoR::DataMgr *dataMgr = dataStatus->GetDataMgr(_dataMgrName);
         _coordVars = dataMgr->GetCoordVarNames();
-        _coordVarsEnabled.resize(_CoordVars.size());
-        std::fill(_CoordVarsEnabled.begin(), _CoordVarsEnabled.end(), false);
+        _coordVarsEnabled.resize(_coordVars.size());
+        std::fill(_coordVarsEnabled.begin(), _coordVarsEnabled.end(), false);
 
         _2DVars = dataMgr->GetDataVarNames(TWOD);
         _2DVarsEnabled.resize(_2DVars.size());
@@ -389,7 +395,7 @@ void PythonVariables::_deleteScript() {
     _controlExec->RemoveFunction(_scriptType, dataMgrName, scriptName);
 
     if (scriptName == _scriptName)
-        reset();
+        _reset();
 
     Update(true);
 }
@@ -532,7 +538,7 @@ void PythonVariables::_saveScript() {
     _justSaved = true;
 }
 
-std::vector<string> _buildInputVars() const {
+std::vector<string> PythonVariables::_buildInputVars() const {
     std::vector<string> inputVars;
     for (int i = 0; i < _coordVars.size(); i++) {
         if (_coordVarsEnabled[i] == true)
@@ -551,7 +557,7 @@ std::vector<string> _buildInputVars() const {
 }
 
 void PythonVariables::_closeScript() {
-    reset();
+    _reset();
     Update(true);
     close();
 }
@@ -636,6 +642,18 @@ void PythonVariables::_3DInputVarChanged(int row, int col) {
         _3DVarsEnabled[row] = false;
 
     Update(true);
+}
+
+void PythonVariables::_coordinatesCheckboxClicked(int state) {
+    if (state == Qt::Unchecked) {
+        cout << "Unchecked" << endl;
+        _variableTabs->removeTab(0);
+        _coordVarsEnabled.resize(_coordVars.size());
+        std::fill(_coordVarsEnabled.begin(), _coordVarsEnabled.end(), false);
+    } else {
+        cout << "checked" << endl;
+        _variableTabs->insertTab(0, _coordTab, "Coordinates");
+    }
 }
 
 void PythonVariables::_createNewVariable() {
@@ -766,7 +784,7 @@ void PythonVariables::_makeInputTableValues(std::vector<string> &tableValuesCoor
                                             std::vector<string> &tableValues3D,
                                             std::vector<string> &summaryValues) const {
     string onOff;
-    for (int i = 0; i < _CoordVars.size(); i++) {
+    for (int i = 0; i < _coordVars.size(); i++) {
         tableValuesCoords.push_back(_coordVars[i]);
 
         onOff = "0";
