a68e16f6a98d96d6af9e4f00fca28e041c0023c0
Samuel Li <shaomeng@ucar.edu>
2018-11-21 23:34:45 -0700

    Isosurface renderer now uses eye space for ray casting now

--- lib/render/IsoSurfaceRenderer.cpp
+++ lib/render/IsoSurfaceRenderer.cpp
@@ -42,11 +42,12 @@ void IsoSurfaceRenderer::_3rdPassSpecialHandling(bool fast, long castingMode) {
 
     // Special handling for IsoSurface: pass in *normalized* iso values.
     std::vector<float> validValues;
-    for (int i = 0; i < isoFlags.size(); i++)
+    for (int i = 0; i < isoFlags.size(); i++) {
         if (isoFlags[i])
             validValues.push_back(
                 (float(isoValues[i]) - _userCoordinates.valueRange[0]) /
                 (_userCoordinates.valueRange[1] - _userCoordinates.valueRange[0]));
+    }
     int numOfIsoValues = (int)validValues.size();
 
     glUniform1i(glGetUniformLocation(_3rdPassShaderId, "numOfIsoValues"), numOfIsoValues);
