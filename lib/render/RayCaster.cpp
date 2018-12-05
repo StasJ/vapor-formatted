7802f7252e65efb17c64969a5111e5f3003b3e40
Samuel Li <shaomeng@ucar.edu>
2018-12-05 00:25:17 -0700

    tweak sampling frequency

--- lib/render/RayCaster.cpp
+++ lib/render/RayCaster.cpp
@@ -892,7 +892,7 @@ void RayCaster::_load3rdPassUniforms(long castingMode, const glm::mat4 &inversed
         stepSize1D = std::sqrt(span[0] * span[0] + span[1] * span[1] + span[2] * span[2]) / 100.0f;
     else
         stepSize1D = std::sqrt(span[0] * span[0] + span[1] * span[1] + span[2] * span[2]) /
-                     float(_userCoordinates.dims[3] * 4);
+                     float(_userCoordinates.dims[3] * 2); // Use Nyquist frequency by default
     if (fast)
         stepSize1D *= 4.0; // Quadruple step size when fast rendering
     uniformLocation = glGetUniformLocation(_3rdPassShaderId, "stepSize1D");
