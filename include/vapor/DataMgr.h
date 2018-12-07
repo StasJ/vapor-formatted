21b64d0b047f68350af2725ac307c56ffc36a3d8
John Clyne <clyne@ucar.edu>
2018-12-07 16:44:42 -0700

    Fixed 1065

--- include/vapor/DataMgr.h
+++ include/vapor/DataMgr.h
@@ -843,9 +843,10 @@ class VDF_API DataMgr : public Wasp::MyBase {
 
     template <typename T>
     int _get_blocked_region_from_fs(size_t ts, string varname, int level, int lod,
-                                    const vector<size_t> &file_bs, const vector<size_t> &grid_dims,
-                                    const vector<size_t> &grid_bs, const vector<size_t> &grid_min,
-                                    const vector<size_t> &grid_max, T *blks);
+                                    const vector<size_t> &file_bs, const vector<size_t> &file_dims,
+                                    const vector<size_t> &grid_dims, const vector<size_t> &grid_bs,
+                                    const vector<size_t> &grid_min, const vector<size_t> &grid_max,
+                                    T *blks);
 
     template <typename T>
     T *_get_region_from_fs(size_t ts, string varname, int level, int lod,
