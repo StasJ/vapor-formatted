31cf6231d2a2085d27ab041b01de1d51cfbbcb62
Samuel Li <shaomeng@ucar.edu>
2018-12-06 22:55:36 -0700

    RayCaster and DVR renderer receives a performance boost

--- include/vapor/RayCaster.h
+++ include/vapor/RayCaster.h
@@ -58,9 +58,8 @@ class RENDER_API RayCaster : public Renderer {
         float *xyCoords;                 // X-Y coordinate values
         float *zCoords;                  // Z coordinate values
 
-        float valueRange[2]; // min and max values of the volume
-        size_t dims[4];      // num. of samples along each axis.
-                             // !! Note: the last element is the diagnal length !!
+        size_t dims[4]; // num. of samples along each axis.
+                        // !! Note: the last element is the diagnal length !!
         float boxMin[3],
             boxMax[3]; // bounding box of the current volume
                        // !! NOTE boxMin and boxMax most likely differ from extents from  params !!
@@ -112,7 +111,7 @@ class RENDER_API RayCaster : public Renderer {
 
     UserCoordinates _userCoordinates;
     std::vector<float> _colorMap;
-    float _colorMapRange[2];
+    float _colorMapRange[3]; // min, max, and diff values.
 
     // OpenGL stuff
     // textures
