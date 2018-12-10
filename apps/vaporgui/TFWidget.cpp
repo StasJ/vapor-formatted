3022af7e25d72e6d3ad807b1e2e86a87a0a66da4
Scott Pearse <pearse@ucar.edu>
2018-12-10 09:54:26 -0700

    Fixed two bugs.  1) slicing histogram was not using sampling method, and 2) MappingFrame was not redrawing after its update and recalculation of histogram

--- apps/vaporgui/TFWidget.cpp
+++ apps/vaporgui/TFWidget.cpp
@@ -117,6 +117,9 @@ void TFWidget::Reinit(TFFlags flags) {
     else
         _mappingFrame->setIsolineSliders(false);
 
+    if (_flags & SAMPLING)
+        _mappingFrame->SetIsSampling(true);
+
     configureSecondaryTransferFunction();
     configureConstantColorControls();
 
@@ -410,10 +413,10 @@ void TFWidget::Update(DataMgr *dataMgr, ParamsMgr *paramsMgr, RenderParams *rPar
     updateMainMappingFrame(); // set mapper func to that of current variable, refresh _rParams etc
     updateSecondaryMappingFrame();
 
-    updateQTWidgets();
+    updateQtWidgets();
 }
 
-void TFWidget::updateQTWidgets() {
+void TFWidget::updateQtWidgets() {
     enableUpdateButtonsIfNeeded();
     updateColorInterpolation();
     updateConstColor();
