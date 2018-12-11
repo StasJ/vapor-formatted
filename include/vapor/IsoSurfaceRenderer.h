228c87e8dac6158c0e84623f7d93514cf6ce5237
Samuel Li <shaomenglee@gmail.com>
2018-12-11 16:25:18 -0700

    consolidate shaders

--- include/vapor/IsoSurfaceRenderer.h
+++ include/vapor/IsoSurfaceRenderer.h
@@ -14,7 +14,7 @@ class RENDER_API IsoSurfaceRenderer : public RayCaster {
     static std::string GetClassType() { return ("IsoSurface"); }
 
   protected:
-    int _loadShaders();
+    int _load3rdPassShaders();
     void _3rdPassSpecialHandling(bool fast, long castMode);
 };
 
