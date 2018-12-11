1ef4cd6b8fa76d3ac7e0d614b29a8ed2629aa09f
Samuel Li <shaomenglee@gmail.com>
2018-12-11 16:25:18 -0700

    consolidate shaders

--- lib/render/DVRenderer.cpp
+++ lib/render/DVRenderer.cpp
@@ -15,18 +15,8 @@ DVRenderer::DVRenderer(const ParamsMgr *pm, std::string &winName, std::string &d
     : RayCaster(pm, winName, dataSetName, DVRParams::GetClassType(), DVRenderer::GetClassType(),
                 instName, dataMgr) {}
 
-int DVRenderer::_loadShaders() {
+int DVRenderer::_load3rdPassShaders() {
     ShaderProgram *shader = nullptr;
-    if ((shader = _glManager->shaderManager->GetShader("DVR1stPass")))
-        _1stPassShader = shader;
-    else
-        return GLERROR;
-
-    if ((shader = _glManager->shaderManager->GetShader("DVR2ndPass")))
-        _2ndPassShader = shader;
-    else
-        return GLERROR;
-
     if ((shader = _glManager->shaderManager->GetShader("DVR3rdPassMode1")))
         _3rdPassMode1Shader = shader;
     else
