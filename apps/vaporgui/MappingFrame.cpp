5c8448abc5e208c0e35e06c4707afaa812edec97
Scott Pearse <pearse@ucar.edu>
2018-12-13 15:27:10 -0700

    GetDataRange diagnostics turned off.  Stride=4 for both the Histogram and DataMgr::GetDataRange()

--- apps/vaporgui/MappingFrame.cpp
+++ apps/vaporgui/MappingFrame.cpp
@@ -59,7 +59,8 @@
 #define YZ 2
 
 #define SAMPLE_RATE 100
-#define FAST_MODE_FACTOR 2
+#define FAST_MODE_FACTOR 4
+#define TIME_ME = false
 
 using namespace VAPoR;
 using namespace std;
@@ -205,14 +206,18 @@ void MappingFrame::RefreshHistogram() {
 
     _histogram = new Histo(256, minRange, maxRange, _variableName, ts);
 
+#ifdef TIME_ME
     std::clock_t start;
     double duration;
     start = std::clock();
+#endif
 
     populateHistogram();
 
+#ifdef TIME_ME
     duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
     std::cout << "Refresh Histogram Time: " << duration << '\n';
+#endif
 
     _histogramMap[rendererName] = _histogram;
 }
@@ -355,6 +360,7 @@ void MappingFrame::updateMapperFunction(MapperFunction *mapper) {
     deleteOpacityWidgets();
 
     _mapper = mapper;
+    _mapper->setHistogramFastMode(true);
 
     if (_opacityMappingEnabled) {
         //
