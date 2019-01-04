1b51968e63e5a5d17e0d0070aa56ab44e45c173b
John Clyne <clyne@ucar.edu>
2019-01-04 15:39:05 -0700

    Fixed #1179 - Accessing non-existent variables causes crash
    Also fixed several other renderers that failed in when variables were missing.

--- lib/render/BarbRenderer.cpp
+++ lib/render/BarbRenderer.cpp
@@ -257,7 +257,6 @@ int BarbRenderer::_getVarGrid(int ts, int refLevel, int lod, string varName,
                 if (varData[i])
                     _dataMgr->UnlockGrid(varData[i]);
             }
-            glEndList();
             return (rc);
         }
         varData[varData.size() - 1] = sg;
@@ -283,7 +282,6 @@ int BarbRenderer::_paintGL(bool) {
     // Get vector variables
     rc = _getVectorVarGrids(ts, refLevel, lod, minExts, maxExts, varData);
     if (rc < 0) {
-        glEndList();
         SetErrMsg("One or more selected field variables does not exist");
         return -1;
     }
