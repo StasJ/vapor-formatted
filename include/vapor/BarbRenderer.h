6c8d890f7206117fa5b64b15d46a71893c63daf8
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-01-03 20:13:51 -0700

    Fixed barb scaling

--- include/vapor/BarbRenderer.h
+++ include/vapor/BarbRenderer.h
@@ -114,12 +114,12 @@ class RENDER_API BarbRenderer : public Renderer {
 
     void _getStrides(vector<float> &strides, vector<int> &rakeGrid, vector<float> &rakeExts) const;
 
-    bool _defineBarb(std::vector<Grid *>, float start[3], float end[3], bool doColorMapping,
-                     float clut[1024]);
+    bool _defineBarb(const std::vector<Grid *>, float start[3], float end[3], bool doColorMapping,
+                     const float clut[1024]);
 
     void _operateOnGrid(vector<Grid *> variableData, bool drawBarb = true);
 
-    bool _getColorMapping(float val, float clut[256 * 4]);
+    bool _getColorMapping(float val, const float clut[256 * 4]);
 
     float _calculateDirVec(const float start[3], const float end[3], float dirVec[3]);
 
@@ -135,8 +135,8 @@ class RENDER_API BarbRenderer : public Renderer {
     //! \param[in] const float startPoint[3] beginning position of barb
     //! \param[in] const float endPoint[3] ending position of barb
     // void drawBarb(const float startPoint[3], const float endPoint[3]);
-    void _drawBarb(const std::vector<Grid *> variableData, float startPoint[3], bool doColorMapping,
-                   float clut[1024]);
+    void _drawBarb(const std::vector<Grid *> variableData, const float startPoint[3],
+                   bool doColorMapping, const float clut[1024]);
 
 #ifdef DEBUG
     _printBackDiameter(const float startVertex[18]) const;
