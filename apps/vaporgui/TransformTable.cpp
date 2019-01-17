67a34b8d06e607aac863f9cafa21207de8ec1202
Scott Pearse <pearse@ucar.edu>
2019-01-17 15:37:07 -0700

    removed comment

--- apps/vaporgui/TransformTable.cpp
+++ apps/vaporgui/TransformTable.cpp
@@ -100,8 +100,6 @@ void TransformTable::UpdateScales() {
         }
     }
 
-    // if (_verticalHeaders.size())
-
     _scaleTable->Update(numDatasets, 3, allScales, _verticalHeaders, _horizontalHeaders);
 }
 
