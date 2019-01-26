39530cfa1edfd5a21bca06b7bde7cb7399c5ff57
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-01-25 23:48:14 -0700

    Added algorithm to gui

--- include/vapor/VolumeParams.h
+++ include/vapor/VolumeParams.h
@@ -17,8 +17,14 @@ class PARAMS_API VolumeParams : public RenderParams {
 
     static string GetClassType() { return ("VolumeParams"); }
 
+    std::string GetAlgorithm() const;
+    void SetAlgorithm(std::string algorithm);
+    static const std::vector<std::string> GetAlgorithmNames();
+
   private:
     void _init();
+
+    static const std::string _algorithmTag;
 };
 
 }; // namespace VAPoR
