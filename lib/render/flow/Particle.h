2ad809163a471ac102c0f0db4b2d7777f899270d
Samuel Li <shaomenglee@gmail.com>
2019-02-20 17:43:30 -0700

    add Stream class

--- lib/render/flow/Particle.h
+++ lib/render/flow/Particle.h
@@ -12,9 +12,9 @@ namespace VAPoR {
 namespace flow {
 class Particle {
   public:
-    glm::vec3 location;
-    glm::vec4 color;
-    std::vector<float> propertiesF;
+    glm::vec3 _location;
+    glm::vec4 _color;
+    std::vector<float> _propertiesF;
 
     // Constructor and destructor
     Particle();
@@ -22,10 +22,9 @@ class Particle {
     Particle(const float *loc);
     Particle(float x, float y, float z);
     ~Particle();
+};
 
-}; // End of class Particle
-
-}; // End of namespace flow
-}; // End of namespace VAPoR
+}; // namespace flow
+}; // namespace VAPoR
 
 #endif
---
+++ lib/render/flow/Stream.cpp
@@ -0,0 +1,8 @@
+#include "Stream.h"
+
+using namespace VAPoR;
+using namespace flow;
+
+Stream::Stream() {}
+
+Stream::~Stream() {}
