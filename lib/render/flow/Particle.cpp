bad429a56bf59a0f7ce34041e18644c3d1f2c7af
Samuel Li <shaomenglee@gmail.com>
2019-02-21 20:59:47 -0700

    add value field for particle

--- lib/render/flow/Particle.cpp
+++ lib/render/flow/Particle.cpp
@@ -2,11 +2,15 @@
 
 using namespace flow;
 
-Particle::Particle() { time = 0.0f; }
+Particle::Particle() {
+    time = 0.0f;
+    value = 0.0f;
+}
 
 Particle::Particle(const glm::vec3 &loc, float t) {
     location = loc;
     time = t;
+    value = 0.0f;
 }
 
 Particle::Particle(const float *loc, float t) {
@@ -14,6 +18,7 @@ Particle::Particle(const float *loc, float t) {
     location.y = loc[1];
     location.z = loc[2];
     time = t;
+    value = 0.0f;
 }
 
 Particle::Particle(float x, float y, float z, float t) {
@@ -21,6 +26,7 @@ Particle::Particle(float x, float y, float z, float t) {
     location.y = y;
     location.z = z;
     time = t;
+    value = 0.0f;
 }
 
 Particle::~Particle() {}
