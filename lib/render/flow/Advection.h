6ab11bb01c2a3845eff5c658ad8ed65af5eb4fc0
Samuel Li <shaomenglee@gmail.com>
2019-02-21 18:46:25 -0700

    add Advection files

---
+++ lib/render/flow/Advection.h
@@ -0,0 +1,33 @@
+/*
+ * This class performances advection calculations.
+ */
+
+#ifndef ADVECTION_H
+#define ADVECTION_H
+
+#include "Particle.h"
+#include "VelocityField.h"
+#include <string>
+
+namespace flow {
+class Advection {
+  public:
+    // Constructor and destructor
+    Advection();
+    ~Advection();
+
+    int advectEuler(float deltaT);
+    int advectRK4(float deltaT);
+
+    void useVelocityField(VelocityField *p);
+    void useSeedParticles(std::vector<Particle> &seeds);
+
+  private:
+    VelocityField *_vField;
+
+    std::vector<std::vector<Particle>> _streams;
+};
+
+}; // namespace flow
+
+#endif
