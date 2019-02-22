a311c75c3f68056b2eb626af7e3bfcf7bf5acc76
Samuel Li <shaomenglee@gmail.com>
2019-02-21 17:37:04 -0700

    implement a base class for velocity field, and a synthetic ocean field class

---
+++ lib/render/flow/OceanField.h
@@ -0,0 +1,22 @@
+/*
+ * A drived VelocityField that simulates the ocean flow.
+ */
+
+#ifndef OCEANFIELD_H
+#define OCEANFIELD_H
+
+#include "VelocityField.h"
+
+namespace flow {
+class OceanField : public VelocityField {
+  public:
+    OceanField();
+    ~OceanField();
+
+    int Get(float time, const glm::vec3 &pos, glm::vec3 &vel);
+    bool insideField(const glm::vec3 &pos);
+};
+
+}; // namespace flow
+
+#endif
