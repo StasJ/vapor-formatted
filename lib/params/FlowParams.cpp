5fcf4526af1aeec1d7b07b1a0c80edc00def6fcc
Samuel Li <shaomenglee@gmail.com>
2019-03-01 14:04:17 -0700

    fix flow params

--- lib/params/FlowParams.cpp
+++ lib/params/FlowParams.cpp
@@ -2,13 +2,25 @@
 
 using namespace VAPoR;
 
-FlowParams::FlowParams(DataMgr *dataManager, ParamsBase::StateSave *stateSave,
-                       std::string classType)
-    : RenderParams(dataManager, stateSave, classType, 3 /* max dim */) {
+// Constructor
+#if 0
+FlowParams::FlowParams(   DataMgr*                dataManager, 
+                          ParamsBase::StateSave*  stateSave,
+                          std::string             classType )
+       : RenderParams(    dataManager, 
+                          stateSave, 
+                          classType,
+                          3 /* max dim */ )
+{
+    SetDiagMsg("FlowParams::FlowParams() this=%p", this);
+}
+#endif
+
+FlowParams::FlowParams(DataMgr *dataManager, ParamsBase::StateSave *stateSave)
+    : RenderParams(dataManager, stateSave, FlowParams::GetClassType(), 3 /* max dim */) {
     SetDiagMsg("FlowParams::FlowParams() this=%p", this);
 }
 
-// Constructor
 FlowParams::FlowParams(DataMgr *dataManager, ParamsBase::StateSave *stateSave, XmlNode *node)
     : RenderParams(dataManager, stateSave, node, 3 /* max dim */) {
     SetDiagMsg("FlowParams::FlowParams() this=%p", this);
