00663c52ba6d3287a39454aeb3a065f7c5b9085d
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-03-06 11:26:58 -0700

    Clip to user extents regular volumes

--- include/vapor/VolumeRenderer.h
+++ include/vapor/VolumeRenderer.h
@@ -25,6 +25,8 @@ class RENDER_API VolumeRenderer : public Renderer {
     int _loadData();
     void _loadTF();
     glm::vec3 _getVolumeScales() const;
+    void _getExtents(glm::vec3 *dataMin, glm::vec3 *dataMax, glm::vec3 *userMin,
+                     glm::vec3 *userMax) const;
 
     VolumeAlgorithm *algorithm;
 
