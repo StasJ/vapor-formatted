04b9eada8e6b13f9a17f8475dcdd39dba4b7acc8
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-03-13 18:10:34 -0600

    cleanup

--- include/vapor/VolumeIsoRenderer.h
+++ include/vapor/VolumeIsoRenderer.h
@@ -1,53 +1,21 @@
 #pragma once
 
 #include <glm/fwd.hpp>
-#include <vapor/Renderer.h>
 #include <vapor/VolumeAlgorithm.h>
+#include <vapor/VolumeRenderer.h>
 
 using std::string;
 using std::vector;
 
 namespace VAPoR {
 
-class RENDER_API VolumeIsoRenderer : public Renderer {
+class RENDER_API VolumeIsoRenderer : public VolumeRenderer {
   public:
     VolumeIsoRenderer(const ParamsMgr *pm, std::string &winName, std::string &dataSetName,
                       std::string &instName, DataMgr *dataMgr);
     ~VolumeIsoRenderer();
 
     static std::string GetClassType() { return ("VolumeIso"); }
-
-  protected:
-    int _initializeGL();
-    int _paintGL(bool fast);
-    void _clearCache(){};
-
-    int _loadData();
-    void _loadTF();
-    glm::vec3 _getVolumeScales() const;
-    void _getExtents(glm::vec3 *dataMin, glm::vec3 *dataMax, glm::vec3 *userMin,
-                     glm::vec3 *userMax) const;
-
-    VolumeAlgorithm *algorithm;
-
-    unsigned int VAO = 0;
-    unsigned int VBO = 0;
-    unsigned int LUTTexture;
-    unsigned int depthTexture;
-
-    struct Cache {
-        string var;
-        size_t ts;
-        int refinement;
-        int compression;
-
-        MapperFunction *tf = nullptr;
-        vector<double> mapRange;
-
-        string algorithmName;
-
-        bool needsUpdate;
-    } cache;
 };
 
 }; // namespace VAPoR
