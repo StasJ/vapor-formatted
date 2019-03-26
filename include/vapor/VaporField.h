c9f1a63d0391a362657aa36a13b1c36c9a91f785
Samuel Li <shaomenglee@gmail.com>
2019-03-26 13:49:38 -0600

    WIP VaporField, move to fix compilation

--- include/vapor/VaporField.h
+++ include/vapor/VaporField.h
@@ -1,7 +1,9 @@
 #ifndef VAPORFIELD_H
 #define VAPORFIELD_H
 
+#include "vapor/DataMgr.h"
 #include "vapor/Field.h"
+#include "vapor/FlowParams.h"
 
 namespace flow {
 class VaporField : public Field {
@@ -9,15 +11,43 @@ class VaporField : public Field {
     VaporField();
     virtual ~VaporField();
 
+    //
+    // Functions from class Field
+    //
     virtual bool InsideVolume(float time, const glm::vec3 &pos) const;
     virtual int GetVelocity(float time, const glm::vec3 &pos, // input
                             glm::vec3 &vel) const;            // output
     virtual int GetScalar(float time, const glm::vec3 &pos,   // input
                           float &val) const;                  // output
-
     virtual int GetNumberOfTimesteps() const;
 
-  protected:
+    //
+    // Functions for interaction with VAPOR components
+    //
+    void AssignDataManager(const VAPoR::DataMgr *dmgr);
+    void UpdateParams(const VAPoR::FlowParams *p);
+
+    //
+    // Find one index whose timestamp is just below a given time
+    // I.e., _timestamps[floor] <= time
+    //
+    int LocateTimestamp(float time,           // Input
+                        size_t &floor) const; // Output
+
+  private:
+    // Member variables
+    std::vector<float> _timestamps; // in ascending order
+    const VAPoR::DataMgr *_datamgr;
+    const VAPoR::FlowParams *_params;
+
+    // Member functions
+    template <typename T>
+    size_t _binarySearch(const std::vector<T> &vec, T val, size_t begin, size_t end) const;
+
+    // _getAGrid will use the cached params, _params, to generate grids.
+    int _getAGrid(int timestep,         // Input
+                  std::string &varName, // Input
+                  Grid **gridpp) const; // Output
 };
 }; // namespace flow
 
