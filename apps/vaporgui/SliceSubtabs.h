d28851503142355dc1fdb8731e3eb8f139694e23
Scott Pearse <pearse@ucar.edu>
2019-04-23 11:34:42 -0600

    Removed test file changes

--- apps/vaporgui/SliceSubtabs.h
+++ apps/vaporgui/SliceSubtabs.h
@@ -28,14 +28,6 @@ class SliceVariablesSubtab : public QWidget, public Ui_SliceVariablesGUI {
 
   private slots:
     void _setDefaultSampleRate();
-    void spinComboChanged() { cout << "spin " << spinBox->GetValue() << endl; }
-    void dSpinComboChanged() { cout << "dspin " << dSpinBox->GetValue() << endl; }
-    void editChanged() { cout << "edit " << edit->GetEditText() << endl; }
-    void veditChanged() { cout << "vedit " << vedit->GetEditText() << endl; }
-    void readFile() { cout << "read " << reader->GetPath() << endl; }
-    void readFileFilter() { cout << "readFilt " << readerFilter->GetPath() << endl; }
-    void writeFile() { cout << "write " << writer->GetPath() << endl; }
-    void writeFileFilter() { cout << "writeFilt " << writerFilter->GetPath() << endl; }
 
   private:
     VAPoR::SliceParams *_params;
