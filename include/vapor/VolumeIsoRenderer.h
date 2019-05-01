7246efa9f7aa1ef920381e0e56f387fcc0ea585f
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-04-30 23:46:29 -0600

    Refactored VolumeRenderer

--- include/vapor/VolumeIsoRenderer.h
+++ include/vapor/VolumeIsoRenderer.h
@@ -17,7 +17,8 @@ class RENDER_API VolumeIsoRenderer : public VolumeRenderer {
 
     static std::string GetClassType() { return ("NEW_IsoSurface"); }
 
-    bool _usingColorMapData() const;
+    virtual bool _usingColorMapData() const;
+    virtual void _setShaderUniforms(const ShaderProgram *shader) const;
     virtual std::string _getDefaultAlgorithmForGrid(const Grid *grid) const;
 };
 
