a968672df150adf800512822cf8414713e745c01
Samuel Li <shaomeng@ucar.edu>
2019-07-16 18:38:12 -0600

    fix signal name mismatch

--- apps/vaporgui/VaporWidgets.h
+++ apps/vaporgui/VaporWidgets.h
@@ -133,7 +133,7 @@ class VLineEdit : public VaporWidget {
     // QValidator* _validator;
 
   private slots:
-    void _returnPressed();
+    void _relaySignal();
 
   private:
     std::string _text;
