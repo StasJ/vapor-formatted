1b7f7e6bba3c9bcb7fd811f80e7766b83a57ecf4
Samuel Li <shaomeng@ucar.edu>
2019-08-23 17:55:21 -0600

    implement biased strength

--- include/vapor/FlowRenderer.h
+++ include/vapor/FlowRenderer.h
@@ -90,7 +90,7 @@ class RENDER_API FlowRenderer : public Renderer {
     int _genSeedsXY(std::vector<flow::Particle> &seeds, float timeVal) const;
     int _genSeedsRakeUniform(std::vector<flow::Particle> &seeds, float timeVal) const;
     int _genSeedsRakeRandom(std::vector<flow::Particle> &seeds, float timeVal) const;
-    // int  _genSeedsRakeRandomBiased( std::vector<flow::Particle>& seeds, float timeVal ) const;
+    int _genSeedsRakeRandomBiased(std::vector<flow::Particle> &seeds, float timeVal) const;
 
     int _renderFromAnAdvection(const flow::Advection *, FlowParams *, bool fast);
     void _prepareColormap(FlowParams *);
