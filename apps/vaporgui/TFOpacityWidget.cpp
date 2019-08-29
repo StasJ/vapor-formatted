7aa7831a8305a5cb0446d830b55b8848cde7b73f
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-08-29 16:14:39 -0600

    Missing updates

--- apps/vaporgui/TFOpacityWidget.cpp
+++ apps/vaporgui/TFOpacityWidget.cpp
@@ -60,6 +60,7 @@ void TFOpacityWidget::Update(VAPoR::DataMgr *dataMgr, VAPoR::ParamsMgr *paramsMg
         _controlPoints[i / 2].y = cp[i];
         _controlPoints[i / 2].x = cp[i + 1];
     }
+    update();
 }
 
 QSize TFOpacityWidget::minimumSizeHint() const { return QSize(100, 75); }
