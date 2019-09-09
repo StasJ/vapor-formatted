6e41bd913221708a7732f9906bed1dd7e9c6c778
Scott Pearse <pearse@ucar.edu>
2019-09-09 13:24:38 -0600

    incremental

--- apps/vaporgui/SliceEventRouter.h
+++ apps/vaporgui/SliceEventRouter.h
@@ -50,8 +50,8 @@ class SliceEventRouter : public QTabWidget, public RenderEventRouter {
     VSpinBox *_vsb;
     PSlider *_psl;
     PSlider *_psl2;
-    PRange *_prange;
-    PGeometry *_pgeometry;
+    VRange *_vrange;
+    VGeometry *_vgeometry;
 
   protected:
     void _updateTab();
