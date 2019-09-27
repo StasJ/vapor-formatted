ef01216ea251d802bbf427a4a982f3ab139b8cc7
Samuel Li <shaomenglee@gmail.com>
2019-09-26 23:20:43 -0600

    basic functions seem complete

--- lib/common/test.cpp
+++ lib/common/test.cpp
@@ -1,4 +1,13 @@
-#include "VaporCache.hpp"
+#include "unique_ptr_cache.hpp"
 #include <string>
 
-int main() { vapor_cache<std::string, int> c1(2); }
+class Obj {
+  public:
+    int val;
+    Obj(int v) : val(v) {}
+};
+
+int main() {
+    unique_ptr_cache<std::string, Obj> c1(5);
+    c1.insert("haha", new Obj(4));
+}
