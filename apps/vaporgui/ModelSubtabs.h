f1a08711eaa6d3d24b8104941081d5635a751747
Stanislaw Jaroszynski <Stanislaw Jaroszynski>
2019-10-08 17:24:58 -0600

    animation

--- apps/vaporgui/ModelSubtabs.h
+++ apps/vaporgui/ModelSubtabs.h
@@ -17,21 +17,23 @@ class DataMgr;
 
 class SpacingCombo;
 class TFEditor;
+class VSection;
+class ParamsWidget;
 
 class ModelVariablesSubtab : public QWidget, public Ui_ModelVariablesGUI {
 
     Q_OBJECT
 
   public:
-    ModelVariablesSubtab(QWidget *parent) {
-        setupUi(this);
-        _variablesWidget->Reinit((VariableFlags)(SCALAR | HEIGHT), (DimFlags)(TWOD));
-    }
+    ModelVariablesSubtab(QWidget *parent);
 
-    void Update(VAPoR::DataMgr *dataMgr, VAPoR::ParamsMgr *paramsMgr,
-                VAPoR::RenderParams *rParams) {
-        _variablesWidget->Update(dataMgr, paramsMgr, rParams);
-    }
+    void Update(VAPoR::DataMgr *dataMgr, VAPoR::ParamsMgr *paramsMgr, VAPoR::RenderParams *rParams);
+
+  private:
+    VSection *_modelSettings;
+    vector<ParamsWidget *> _pw;
+
+    void addPW(ParamsWidget *w);
 };
 
 class ModelAppearanceSubtab : public QWidget, public Ui_ModelAppearanceGUI {
