a06f060374e016523ca29e62c215e53304a3b201
sgpearse <pearse@ucar.edu>
2019-10-15 12:12:59 -0600

    Fix for broken windows build (#1850)
    * Fix for broken windows build
    
    * Updated previous windows build fix, and fixed more windows build problems
    
    * messed up merge
    
    * updates
    
    * updates
    
    * Added Sam's conditional for template specialization

--- apps/vaporgui/VizWin.cpp
+++ apps/vaporgui/VizWin.cpp
@@ -19,6 +19,7 @@
 //
 //		Supports mouse event reporting.
 //
+
 #include "VizWin.h"
 #include "AnimationParams.h"
 #include "Core3_2_context.h"
@@ -720,8 +721,8 @@ VAPoR::Transform *VizWin::_getDataMgrTransform() const {
 
 void VizWin::updateManip(bool initialize) {
 
-    std::vector<double> minExts(3, numeric_limits<double>::max());
-    std::vector<double> maxExts(3, numeric_limits<double>::lowest());
+    std::vector<double> minExts(3, (std::numeric_limits<double>::max)());
+    std::vector<double> maxExts(3, std::numeric_limits<double>::lowest());
 
     _getActiveExtents(minExts, maxExts);
 
