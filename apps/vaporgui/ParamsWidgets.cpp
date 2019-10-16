29e4944f44e03d432a0d10ebff7581717ddafdf3
Stanislaw Jaroszynski <Stanislaw Jaroszynski>
2019-10-15 18:00:47 -0600

    Improve ParamsWidgetFile layout

--- apps/vaporgui/ParamsWidgets.cpp
+++ apps/vaporgui/ParamsWidgets.cpp
@@ -5,7 +5,6 @@
 #include <QHBoxLayout>
 #include <QIntValidator>
 #include <QLabel>
-#include <QSpacerItem>
 
 using namespace VAPoR;
 
@@ -23,10 +22,10 @@ ParamsWidget::ParamsWidget(const std::string &tag, const std::string &label) {
     this->setLayout(layout);
 
     QLabel *labelWidget = new QLabel(_label.c_str());
-    QSpacerItem *spacer = new QSpacerItem(108, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+    _spacer = new QSpacerItem(108, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
 
     layout->addWidget(labelWidget);
-    layout->addItem(spacer);
+    layout->addItem(_spacer);
 }
 
 ParamsWidgetCheckbox::ParamsWidgetCheckbox(const std::string &tag, const std::string &labelText)
@@ -190,14 +189,18 @@ std::vector<double> ParamsWidgetColor::QColorToVector(const QColor &c) {
 
 ParamsWidgetFile::ParamsWidgetFile(const std::string &tag, const std::string &label)
     : ParamsWidget(tag, label) {
+#warning _spacer is a hack. Will be refactored
+    _spacer->changeSize(0, 0, QSizePolicy::Minimum, QSizePolicy::Minimum);
+
     _button = new QPushButton;
     _button->setText("Select");
     connect(_button, SIGNAL(clicked()), this, SLOT(_buttonClicked()));
-    layout()->addWidget(_button);
 
     _pathTexbox = new QLineEdit;
     _pathTexbox->setReadOnly(true);
+
     layout()->addWidget(_pathTexbox);
+    layout()->addWidget(_button);
 }
 
 void ParamsWidgetFile::Update(VAPoR::ParamsBase *p) {
