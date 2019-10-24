5e9464df9e46e4cc545371039629a7db376bb279
Scott Pearse <pearse@ucar.edu>
2019-10-24 10:14:36 -0600

    Incremental

--- lib/common/ResourcePath.cpp
+++ lib/common/ResourcePath.cpp
@@ -115,5 +115,6 @@ std::string Wasp::GetPythonDir() {
 
     if (!FileUtils::Exists(exists))
         path = string(PYTHON_DIR);
+    cout << "ResourcePath.cpp setting PYTHONDIR to " << path << endl;
     return path;
 }
