19ade30fd5155c883169ec062a88a64061ce91f6
StasJ <StasJ@users.noreply.github.com>
2019-10-27 10:05:54 -0600

    Rake region selector (#1881)
    * Rake region selector
    
    * Use GUIStateParams instead of flow params
    
    * Sync selected flow tab with GUIStateParams
    
    * Rename isSeedTabActive to isFlowSeedTabActive

--- apps/vaporgui/FlowEventRouter.cpp
+++ apps/vaporgui/FlowEventRouter.cpp
@@ -44,6 +44,7 @@ FlowEventRouter::FlowEventRouter(QWidget *parent, ControlExec *ce)
     qsseed->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     qsseed->setWidget(_seeding);
     qsseed->setWidgetResizable(true);
+    _seedingTab = qsseed;
     addTab(qsseed, "Flow Settings");
 
     _geometry = new FlowGeometrySubtab(this);
@@ -95,6 +96,23 @@ void FlowEventRouter::_updateTab() {
     _seeding->Update(GetActiveDataMgr(), _controlExec->GetParamsMgr(), GetActiveParams());
     _geometry->Update(_controlExec->GetParamsMgr(), GetActiveDataMgr(), GetActiveParams());
     _annotation->Update(_controlExec->GetParamsMgr(), GetActiveDataMgr(), GetActiveParams());
+
+    // Sync selected tab with GUIStateParams
+    GUIStateParams *gp =
+        (GUIStateParams *)_controlExec->GetParamsMgr()->GetParams(GUIStateParams::GetClassType());
+    if (gp->IsFlowSeedTabActive()) {
+        if (currentWidget() != _seedingTab) {
+            blockSignals(true);
+            setCurrentWidget(_seedingTab);
+            blockSignals(false);
+        }
+    } else {
+        if (currentWidget() == _seedingTab) {
+            blockSignals(true);
+            setCurrentIndex(0);
+            blockSignals(false);
+        }
+    }
 }
 
 string FlowEventRouter::_getDescription() const {
