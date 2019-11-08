c3cc2814f5a7af1d53ed1773c7cedc14424488ee
Scott Pearse <pearse@ucar.edu>
2019-11-08 14:16:14 -0700

    incremental

--- apps/vaporgui/SliceSubtabs.cpp
+++ apps/vaporgui/SliceSubtabs.cpp
@@ -1,5 +1,7 @@
 #include "SliceSubtabs.h"
 #include "TFEditor.h"
+#include "VComboBox.h"
+#include "VLineItem.h"
 
 #define MIN_SAMPLES 1
 #define MAX_SAMPLES 2000
@@ -23,6 +25,10 @@ SliceVariablesSubtab::SliceVariablesSubtab(QWidget *parent) {
     connect(fidelityButtons, SIGNAL(buttonClicked(int)), this, SLOT(_setDefaultSampleRate()));
     QComboBox *refinementCombo = _variablesWidget->_fidelityWidget->refinementCombo;
     connect(refinementCombo, SIGNAL(currentIndexChanged(int)), this, SLOT(_setDefaultSampleRate()));
+
+    std::vector<std::string> values = {"foo", "bar", "baz"};
+    VLineItem *le = new VLineItem("Test", new VComboBox2(values));
+    layout()->addWidget(le);
 }
 
 void SliceVariablesSubtab::Update(VAPoR::DataMgr *dataMgr, VAPoR::ParamsMgr *paramsMgr,
