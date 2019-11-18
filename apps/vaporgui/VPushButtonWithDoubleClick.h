13bfa1bcae43e69aeb116ea67ade627823884df9
Scott Pearse <pearse@ucar.edu>
2019-11-18 14:57:40 -0700

    added files

---
+++ apps/vaporgui/VPushButtonWithDoubleClick.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class VPushButtonWithDoubleClick : public QPushButton {
+    Q_OBJECT
+    using QPushButton::QPushButton;
+    void mouseDoubleClickEvent(QMouseEvent *e) { emit doubleClicked(); }
+
+  signals:
+    void doubleClicked();
+};
