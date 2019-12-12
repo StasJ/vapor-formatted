0f1d4cdcfa0c91d73810f5607496c485fc74e2de
StasJ <StasJ@users.noreply.github.com>
2019-12-11 18:54:29 -0700

    Fix fopen calls (#1985)

--- apps/wasp2raw/wasp2raw.cpp
+++ apps/wasp2raw/wasp2raw.cpp
@@ -113,7 +113,7 @@ template <class T> void CopyVar(string ncdffile, string datafile, T dummy) {
     if (rc < 0)
         exit(1);
 
-    FILE *fp = fopen(datafile.c_str(), "w");
+    FILE *fp = fopen(datafile.c_str(), "wb");
     if (!fp) {
         MyBase::SetErrMsg("fopen(%s) : %M", datafile.c_str());
         exit(1);
