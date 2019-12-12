61e1201e7bd79e4a2c8e1b486e2a75c24a52e696
Scott Pearse <pearse@ucar.edu>
2019-12-12 16:27:45 -0700

    fixed static cast

--- apps/vaporgui/VContainer.cpp
+++ apps/vaporgui/VContainer.cpp
@@ -20,7 +20,7 @@ MouseWheelWidgetAdjustmentGuard::MouseWheelWidgetAdjustmentGuard(QObject *parent
     : QObject(parent) {}
 
 bool MouseWheelWidgetAdjustmentGuard::eventFilter(QObject *o, QEvent *e) {
-    const QWidget *widget = static_cast<QWidget *>(o);
+    const QWidget *widget = dynamic_cast<QWidget *>(o);
     if (e->type() == QEvent::Wheel && widget && !widget->hasFocus()) {
         e->ignore();
         return true;
