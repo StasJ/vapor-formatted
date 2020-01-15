f6c0f5de4f83d5787895b95da27df714359d107f
sgpearse <pearse@ucar.edu>
2020-01-15 14:36:32 -0700

    Added dialogs for testing and saving python scripts, in lieu of chang… (#2112)
    * Added dialogs for testing and saving python scripts, in leiu of changing label palettes
    
    * Fix tabs

--- apps/vaporgui/PythonVariables.cpp
+++ apps/vaporgui/PythonVariables.cpp
@@ -49,14 +49,6 @@ PythonVariables::PythonVariables(QWidget *parent) : QDialog(parent), Ui_PythonVa
 
     _justSaved = false;
 
-    QColor background = palette().color(QWidget::backgroundRole());
-    QPalette labelPalette = _scriptSaveLabel->palette();
-    labelPalette.setColor(_scriptSaveLabel->foregroundRole(), background);
-    _scriptSaveLabel->setPalette(labelPalette);
-    labelPalette = _scriptTestLabel->palette();
-    labelPalette.setColor(_scriptTestLabel->foregroundRole(), background);
-    _scriptTestLabel->setPalette(labelPalette);
-
     string pythonImagePath = Wasp::GetSharePath(string("images") + string("/PythonLogo.png"));
 
     QPixmap thumbnail(pythonImagePath.c_str());
@@ -174,41 +166,6 @@ void PythonVariables::Update(bool internalUpdate) {
     _3DInputVarTable->blockSignals(false);
     _summaryTable->blockSignals(false);
     _outputVarTable->blockSignals(false);
-
-    if (_justSaved && internalUpdate) {
-        bool fadeIn = false;
-        _showTestLabel(fadeIn);
-        _showSaveLabel(fadeIn);
-        _justSaved = false;
-    }
-}
-
-void PythonVariables::_showTestLabel(bool show) {
-    QColor textColor;
-    if (show)
-        textColor = QColor(0, 0, 255);
-    else
-        textColor = _scriptTestLabel->palette().color(QPalette::WindowText);
-
-    QPalette labelPalette = _scriptTestLabel->palette();
-    labelPalette.setColor(_scriptTestLabel->foregroundRole(), textColor);
-    _scriptTestLabel->setPalette(labelPalette);
-
-    return;
-}
-
-void PythonVariables::_showSaveLabel(bool show) {
-    QColor textColor;
-    if (show)
-        textColor = QColor(0, 0, 255);
-    else
-        textColor = _scriptSaveLabel->palette().color(QPalette::WindowText);
-
-    QPalette labelPalette = _scriptSaveLabel->palette();
-    labelPalette.setColor(_scriptSaveLabel->foregroundRole(), textColor);
-    _scriptSaveLabel->setPalette(labelPalette);
-
-    return;
 }
 
 void PythonVariables::_connectWidgets() {
@@ -506,17 +463,15 @@ void PythonVariables::_testScript() {
     // Get any output from script
     //
     string s = _controlExec->GetFunctionStdout(_scriptType, _dataMgrName, _scriptName);
+    QMessageBox msgBox;
     if (!s.empty()) {
-        QMessageBox msgBox;
         msgBox.setText("Script output:");
         msgBox.setInformativeText(s.c_str());
         msgBox.exec();
     }
 
-    bool fadeIn = true;
-    _showTestLabel(fadeIn);
-
-    _justSaved = true;
+    msgBox.setText("Test passed.");
+    msgBox.exec();
 }
 
 void PythonVariables::_saveScript() {
@@ -532,9 +487,9 @@ void PythonVariables::_saveScript() {
         return;
     }
 
-    bool fadeIn = true;
-    _showSaveLabel(fadeIn);
-    _justSaved = true;
+    QMessageBox msgBox;
+    msgBox.setText("Script saved to session.");
+    msgBox.exec();
 }
 
 std::vector<string> PythonVariables::_buildInputVars() const {
@@ -554,33 +509,6 @@ std::vector<string> PythonVariables::_buildInputVars() const {
 void PythonVariables::_closeScript() {
     _reset();
     close();
-}
-
-void PythonVariables::_saveToSession() {
-    bool fadeIn = true;
-    _showSaveLabel(fadeIn);
-    _justSaved = true;
-}
-
-void PythonVariables::_updateSaveLabelColor(int r, int g, int b) {
-    QColor newColor = QColor(r, g, b);
-    QPalette labelPalette = _scriptSaveLabel->palette();
-    labelPalette.setColor(_scriptSaveLabel->foregroundRole(), newColor);
-    _scriptSaveLabel->setPalette(labelPalette);
-}
-
-void PythonVariables::_updateTestLabelColor(int r, int g, int b) {
-    QColor newColor = QColor(r, g, b);
-    QPalette labelPalette = _scriptTestLabel->palette();
-    labelPalette.setColor(_scriptTestLabel->foregroundRole(), newColor);
-    _scriptTestLabel->setPalette(labelPalette);
-}
-
-void PythonVariables::_updateLabelColor(int r, int g, int b, QLabel *label) {
-    QColor newColor = QColor(r, g, b);
-    QPalette labelPalette = label->palette();
-    labelPalette.setColor(label->foregroundRole(), newColor);
-    label->setPalette(labelPalette);
 }
 
 void PythonVariables::_coordInputVarChanged(int row, int col) {
@@ -788,13 +716,7 @@ void PythonVariables::_deleteVariable() {
     Update(true);
 }
 
-void PythonVariables::_scriptChanged() {
-    _script = _scriptEdit->toPlainText().toStdString();
-
-    bool fadeIn = false;
-    _showTestLabel(fadeIn);
-    _showSaveLabel(fadeIn);
-}
+void PythonVariables::_scriptChanged() { _script = _scriptEdit->toPlainText().toStdString(); }
 
 void PythonVariables::_makeInputTableValues(std::vector<string> &tableValuesCoords,
                                             std::vector<string> &tableValues2D,
