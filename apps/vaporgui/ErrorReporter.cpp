b46c555edda7c59534d2d3f2445db586af38fb42
StasJ <StasJ@users.noreply.github.com>
2020-01-21 10:43:22 -0700

    Add commit hash to error report (#2130)
    * add commit hash to error report
    
    * version bump

--- apps/vaporgui/ErrorReporter.cpp
+++ apps/vaporgui/ErrorReporter.cpp
@@ -160,6 +160,11 @@ void ErrorReporter::Report(string msg, Type severity, string details) {
             e->_log.pop_back();
         }
     }
+
+    const string sysInfo = GetSystemInformation();
+    details += "------------------\n";
+    details += sysInfo + "\n";
+
     box->setDetailedText(details.c_str());
 
     switch (severity) {
@@ -177,7 +182,7 @@ void ErrorReporter::Report(string msg, Type severity, string details) {
     }
 
     string logText;
-    logText = GetSystemInformation() + "\n";
+    logText = sysInfo + "\n";
     logText += "-------------------\n";
     logText += msg + "\n";
     logText += "-------------------\n";
