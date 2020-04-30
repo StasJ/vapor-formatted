b5adfbead4055d2a1058507b7e130d1cb92886c2
Scott Pearse <pearse@ucar.edu>
2020-04-30 12:27:06 -0600

    incremental

--- apps/vaporgui/VariablesWidget2.cpp
+++ apps/vaporgui/VariablesWidget2.cpp
@@ -2,6 +2,7 @@
 
 #include "PDisplay.h"
 
+#include "FidelityWidget2.h"
 #include "PEnumDropdownHLI.h"
 #include "PGroup.h"
 #include "PVariableSelectorHLI.h"
@@ -12,8 +13,9 @@
 #include <QLabel>
 #include <QLayout>
 
-VariablesWidget2::VariablesWidget2()
-    : VSection("Variable Selection"), _activeDim(3), _initialized(false) {
+const std::string VariablesWidget2::_sectionTitle = "Variable Selection";
+
+VariablesWidget2::VariablesWidget2() : VSection(_sectionTitle), _activeDim(3), _initialized(false) {
     _dimCombo = new VComboBox({"3D", "2D"});
     _dimLineItem = new VLineItem("Variable Dimension", _dimCombo);
     layout()->addWidget(_dimLineItem);
@@ -59,6 +61,9 @@ VariablesWidget2::VariablesWidget2()
     _pg->Add(_heightCombo);
 
     layout()->addWidget(_pg);
+
+    _fidelityWidget = new FidelityWidget2(this);
+    layout()->addWidget(_fidelityWidget);
 
     //
     // Cannot add widgets directly to the layout because it's not vertical.
@@ -202,7 +207,8 @@ void VariablesWidget2::Update(
     _paramsMgr = paramsMgr;
     _rParams = rParams;
 
-    _pg->Update(_rParams, paramsMgr, _dataMgr);
+    _pg->Update(_rParams, _paramsMgr, _dataMgr);
+    _fidelityWidget->Update(_dataMgr, _paramsMgr, _rParams);
     /*_scalarCombo->Update( _rParams, _paramsMgr, _dataMgr );
     _xFieldCombo->Update( _rParams, _paramsMgr, _dataMgr );
     _yFieldCombo->Update( _rParams, _paramsMgr, _dataMgr );
--- apps/vaporgui/VariablesWidget2.h
+++ apps/vaporgui/VariablesWidget2.h
@@ -14,6 +14,7 @@ class PVariableSelector;
 // class PVariableSelectorHLI;
 class VComboBox;
 class VLineItem;
+class FidelityWidget2;
 
 class VariablesWidget2 : public VSection {
 
@@ -51,6 +52,10 @@ class VariablesWidget2 : public VSection {
     PVariableSelector *_zFieldCombo;
     PVariableSelector *_colorCombo;
     PVariableSelector *_heightCombo;
+
+    FidelityWidget2 *_fidelityWidget;
+
+    static const std::string _sectionTitle;
 
     /*
     /Users/pearse/VAPOR/apps/vaporgui/PVariableSelectorHLI.h:13:7:
