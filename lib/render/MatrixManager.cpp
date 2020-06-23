3b812895a89134dfda2a90ca6e71ee38e53a4e20
StasJ <StasJ@users.noreply.github.com>
2020-06-23 08:43:52 -0600

    Flow rendering refactor (#2280)
    * Flow rendering refactor
    
    * fix
    
    * Address GUI review comments
    
    * changes from second gui review meeting
    
    * Added documentation to shaders
    
    * Made var private
    
    * misc
    
    * misc
    
    * Line width
    
    * remove goto

--- lib/render/MatrixManager.cpp
+++ lib/render/MatrixManager.cpp
@@ -74,6 +74,7 @@ void MatrixManager::Rotate(float angle, float x, float y, float z) {
 
 void MatrixManager::Perspective(float fovy, float aspect, float zNear, float zFar) {
     top() = glm::perspective(fovy, aspect, zNear, zFar);
+    _projectionAspectRatio = aspect;
 
 #ifndef NDEBUG
     Near = zNear;
@@ -83,6 +84,7 @@ void MatrixManager::Perspective(float fovy, float aspect, float zNear, float zFa
 
 void MatrixManager::Ortho(float left, float right, float bottom, float top) {
     this->top() = glm::ortho(left, right, bottom, top);
+    _projectionAspectRatio = (right - left) / (top - bottom);
 }
 
 void MatrixManager::Ortho(float left, float right, float bottom, float top, float zNear,
@@ -98,6 +100,11 @@ glm::vec2 MatrixManager::ProjectToScreen(const glm::vec3 &v) const {
     vec4 vs = GetModelViewProjectionMatrix() * vec4(v, 1.0f);
     vs /= vs.w;
     return vs;
+}
+
+float MatrixManager::GetProjectionAspectRatio() const {
+    VAssert(_projectionAspectRatio != 0);
+    return _projectionAspectRatio;
 }
 
 #ifndef NDEBUG
